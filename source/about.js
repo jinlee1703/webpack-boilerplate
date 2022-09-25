ce4561cdb3ba2eb8be8317ba6047f41fc25b925f
Jinwoo Lee <binarywooo@gmail.com>
2022-09-26 02:43:36 +0900

    webpack multiple input & output

--- source/about.js
+++ source/about.js
@@ -2,4 +2,4 @@ import hello_word from "./hello";
 import world_word from "./world.js";
 import css from "./style.css";
 
-document.querySelector('#root').innerHTML = `index: ${hello_word} ${world_word}`;
+document.querySelector('#root').innerHTML = `about: ${hello_word} ${world_word}`;
